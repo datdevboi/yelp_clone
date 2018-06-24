3fa922977003cfb00af87aa60ddea2872673adce
camilo17 <cami172013@gmail.com>
2018-06-23 19:01:26 -0500

    used MapView from expo

---
+++ src/components/containers/Map.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { MapView } from 'expo';
+
+export default class Map extends Component {
+  render() {
+    return (
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    );
+  }
+}
