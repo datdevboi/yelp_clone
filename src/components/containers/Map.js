884f09484295428ed49600eca23d31f686d51614
camilo17 <cami172013@gmail.com>
2018-06-23 21:06:40 -0500

    Added permission and getting location

--- src/components/containers/Map.js
+++ src/components/containers/Map.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
-import { MapView } from 'expo';
+import { MapView, Permissions, Location } from 'expo';
 
 export default class Map extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      location: {
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      },
+      errorMessage: null,
+      permissionGranted: false,
+    };
+  }
+
+  componentDidMount() {
+    this.getPermission();
+  }
+
+  getPermission = async () => {
+    const response = await Permissions.askAsync(Permissions.LOCATION);
+
+    if (response.status === 'granted') {
+      let location = await Location.getCurrentPositionAsync({
+        enableHighAccuracy: true,
+      });
+      // this.setState({ permissionGranted: true, location: location.coords });
+    }
+  };
   render() {
-    return (
-      <MapView
-        style={{ flex: 1 }}
-        initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-      />
-    );
+    return <MapView style={{ flex: 1 }} initialRegion={this.state.location} />;
   }
 }
